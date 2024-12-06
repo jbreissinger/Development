6f9d9c3d14f0f700590c0ddffbffca39346b69f8
jbreissinger <jbreissinger@lahzo.com>
2024-12-06 16:47:48 -0500

    Try generating generic docs

---
+++ documentation-test/dummy.ts
@@ -0,0 +1,6 @@
+/**
+ * Here is some description for the the funciton that I have written just to test the autodoc functionality
+ */
+export async function dummyfunc(message: string){
+    console.log(message)
+}
